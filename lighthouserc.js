aeea055528e891d910ba67df805e7b9b76508584
SarahWanjiru <muhiawanjiru4@gmail.com>
2026-02-04 06:55:17 +0300

    ci: add lighthouserc settings for numberOfRuns and chromeFlags
    Add settings to lighthouserc.js to configure numberOfRuns to 1 and chromeFlags to --no-sandbox --disable-setuid-sandbox. Also update PR build workflow in .github/workflows/Build PR.yml.

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,6 +2,10 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './build',
+      numberOfRuns: 1,
+      settings: {
+        chromeFlags: '--no-sandbox --disable-setuid-sandbox',
+      },
     },
   },
 };
